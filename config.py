c989032cdb877286ccea6c72d51a538565f700a5
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-02-04 20:30:28 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bot_token = "1673411552:AAFLmadz0_ESKkoYM_O_kEiRydpK7Ov_d48"
+bot_token = "1559250481:AAHBYYEtcaH3pP_n9BGacFL3KOVZINFE3-s"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001367838165
 owner_id = 1131653685
